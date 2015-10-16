dcc7a1f6436046de7c2828cb9ea9dd4dafd43a47
Kendra Ash <kendra.ash32@gmail.com>
2015-10-16 11:20:36 -0700

    add edit and delete for tag class

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -23,6 +23,28 @@ class TagsController < ApplicationController
    end
  end
 
+ def edit
+    @tag = Tag.find(params[:id])
+    render :edit
+  end
+
+ def update
+   @tag= Tag.find(params[:id])
+   if @tag.update(tag_params)
+     redirect_to tags_path
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @tag = Tag.find(params[:id])
+   @tag.destroy
+   redirect_to tags_path
+ end
+
+
+
  private
   def tag_params
     params.require(:tag).permit(:name)
